b110fc660065cf3133aaaac580181ffe522773e7
Kartik Maheshwari <95483381+Kartik-Maheshwari@users.noreply.github.com>
2023-08-16 17:53:32 +0530

    MiniPro: 8

---
+++ MiniProjects/P8_ModalProject/app.js
@@ -0,0 +1,15 @@
+// select modal-btn,modal-overlay,close-btn
+// listen for click events on modal-btn and close-btn
+// when user clicks modal-btn add .open-modal to modal-overlay
+// when user clicks close-btn remove .open-modal from modal-overlay
+const modalbtn = document.querySelector(".modal-btn");
+const modaloverlay = document.querySelector(".modal-overlay");
+const closebtn = document.querySelector(".close-btn");
+
+modalbtn.addEventListener("click",()=>{
+    modaloverlay.classList.toggle("open-modal");
+})
+
+closebtn.addEventListener("click",()=>{
+    modaloverlay.classList.toggle("open-modal");
+})
